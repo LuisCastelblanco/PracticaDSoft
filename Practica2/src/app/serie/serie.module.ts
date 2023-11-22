983938d9ec90444394f3842dd33c57089a97f47b
Luis Castelblanco <la.castelblanco@uniandes.edu.co>
2023-11-21 21:03:43 -0500

    Fix bug in login functionality

---
+++ Practica2/src/app/serie/serie.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SerieComponent } from './serie.component';
+import { SerieDetailComponent } from './serie-detail/serie-detail.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SerieComponent, SerieDetailComponent],
+  exports: [SerieComponent, SerieDetailComponent]
+})
+export class SerieModule { }
